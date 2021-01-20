b3d4d9b9e1d5044900f69f002c6fcf3420150a10
Donly Wilson <donly6@hotmail.com>
2021-01-20 08:02:52 -0400

    Add widget functionality

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+//Create function 
 function mytheme_post_thumbnails()
 {
     //Add functionality for featured images
@@ -6,7 +7,31 @@ function mytheme_post_thumbnails()
     //Add functionality for excerpt
     add_post_type_support('page', 'excerpt');
 }
+//Add function to wordpress
 add_action(
     'after_setup_theme',
     'mytheme_post_thumbnails'
 );
+
+function myportfolio_widgets_init()
+{
+    register_sidebar(array(
+        'name'          => __('right-sidebar', 'My Agency'),
+        'id'            => 'sidebar-1',
+        'before_widget' => '<div id="%1$s" class="widget-box %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ));
+
+    register_sidebar(array(
+        'name'          => __('left-sidebar', 'My Agency'),
+        'id'            => 'left-sidebar-1',
+        'before_widget' => '<div id="%1$s" class="widget-box %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ));
+}
+//Add function to wordpress
+add_action('widgets_init', 'myportfolio_widgets_init');
