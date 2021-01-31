79ec357849a10763541afa7e33ed4e7c8075482d
Donly Wilson <donly6@hotmail.com>
2021-01-30 23:33:41 -0400

    start building about and connect css

--- about-page.php
+++ about-page.php
@@ -2,23 +2,29 @@
 /*
 Template Name: About-Page
 */
-
 get_header();
 ?>
 
-<div class="team">
-    <h3>Our Amazing Team</h3>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita iste commodi nisi.</p>
-    <div class="team-galarry">
-        <div class="team-gallery_photo">
-            <!-- <img src="" alt=""> -->
-            <div class="photo"></div>
-            <div class="photo-info">
-                <span class="name">Macy Killy</span>
-                <span class="profestion">Designer</span>
+<div id="about">
+    <div class="container">
+        <div class="team">
+            <div class="team-header">
+                <h3>Our Amazing Team</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita iste commodi nisi.</p>
+            </div>
+            <div class="team-gallery">
+                <div class="team-gallery_photo">
+                    <!-- <img src="" alt=""> -->
+                    <div class="photo"></div>
+                    <div class="photo-info">
+                        <span class="name">Macy Killy</span>
+                        <span class="profestion">Designer</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
 <!-- Wordpress syntax to add footer using php  -->
 <?php get_footer(); ?>
