d5d3db634b3c577f224dec03d0075b8ec18a04e8
Donly Wilson <donly6@hotmail.com>
2021-02-01 19:24:14 -0400

    start work on form

--- footer.php
+++ footer.php
@@ -1,20 +1,44 @@
 <footer>
-    <div class="social-container">
-        <a href="http://www.facebook.com/">
-            <i class="fab fa-facebook"></i>
-        </a>
-        <a href="http://www.dribbble.com/">
-            <i class="fab fa-dribbble"></i>
-        </a>
-        <a href="http://www.facebook.com/">
-            <i class="fab fa-twitter"></i>
-        </a>
-        <a href="http://www.facebook.com/">
-            <i class="fab fa-instagram"></i>
-        </a>
+    <div class="title">
+        <h1>Contact</h1>
     </div>
-    <div class="form">
-        <input type="text">
+    <div class="contact">
+        <div class="contact-info">
+            <div class="email">
+                <span>Email</span>
+                <span>newera@agencey.com</span>
+            </div>
+            <div class="phone">
+                <span>Phone</span>
+                <span>+1 758 230 9214</span>
+            </div>
+            <div class="address">
+                <span>Address</span>
+                <span>Maigot bay, Kanel, 5th</span>
+            </div>
+        </div>
+        <div class="social-container">
+            <a href="http://www.facebook.com/">
+                <i class="fab fa-facebook"></i>
+            </a>
+            <a href="http://www.dribbble.com/">
+                <i class="fab fa-dribbble"></i>
+            </a>
+            <a href="http://www.facebook.com/">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="http://www.facebook.com/">
+                <i class="fab fa-instagram"></i>
+            </a>
+        </div>
+    </div>
+    <div class="contact-form">
+        <!-- contact 7 form -->
+        <?php
+        // echo do_shortcode(
+        //     '[contact-form-7 id="12" title="Contact form 1"]'
+        // );
+        ?>
     </div>
     <h5>Donly Wilson</h5>
     <h6>Web Developer</h6>
