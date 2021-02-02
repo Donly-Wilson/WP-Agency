8a77b487346a360c7ac84d779702ec7df497ac8c
Donly Wilson <donly6@hotmail.com>
2021-02-01 20:00:10 -0400

    styling footer with form

--- footer.php
+++ footer.php
@@ -16,29 +16,39 @@
                 <span>Address</span>
                 <span>Maigot bay, Kanel, 5th</span>
             </div>
+            <div class="social-container">
+                <a href="http://www.facebook.com/">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a href="http://www.dribbble.com/">
+                    <i class="fab fa-dribbble"></i>
+                </a>
+                <a href="http://www.facebook.com/">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="http://www.facebook.com/">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
         </div>
-        <div class="social-container">
-            <a href="http://www.facebook.com/">
-                <i class="fab fa-facebook"></i>
-            </a>
-            <a href="http://www.dribbble.com/">
-                <i class="fab fa-dribbble"></i>
-            </a>
-            <a href="http://www.facebook.com/">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="http://www.facebook.com/">
-                <i class="fab fa-instagram"></i>
-            </a>
+        <div class="contact-form">
+            <!-- contact 7 form -->
+            <?php
+            // echo do_shortcode(
+            //     '[contact-form-7 id="12" title="Contact form 1"]'
+            // );
+            ?>
+            <label for="name">Name<input type="text"></label>
+            <label for="email">Email<input type="text"></label>
+            <label for="phone">Phone<input type="text"></label>
+            <label for="company">Company<input type="text"></label>
+            <label for="website">Website<input type="text"></label>
+            <label for="message">
+                message
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </label>
+            <input type="submit" name="" id="">
         </div>
-    </div>
-    <div class="contact-form">
-        <!-- contact 7 form -->
-        <?php
-        // echo do_shortcode(
-        //     '[contact-form-7 id="12" title="Contact form 1"]'
-        // );
-        ?>
     </div>
     <h5>Donly Wilson</h5>
     <h6>Web Developer</h6>
