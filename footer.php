15bc2c8b2be1eb8722b62d5faecbe6963442f95c
Donly Wilson <donly6@hotmail.com>
2021-02-01 23:19:37 -0400

    better form layout

--- footer.php
+++ footer.php
@@ -31,24 +31,46 @@
                 </a>
             </div>
         </div>
-        <div class="contact-form">
+        <form class="contact-form">
+            <h3>Contact Us</h3>
             <!-- contact 7 form -->
             <?php
             // echo do_shortcode(
             //     '[contact-form-7 id="12" title="Contact form 1"]'
             // );
             ?>
-            <label for="name">Name<input type="text"></label>
-            <label for="email">Email<input type="text"></label>
-            <label for="phone">Phone<input type="text"></label>
-            <label for="company">Company<input type="text"></label>
-            <label for="website">Website<input type="text"></label>
-            <label for="message">
-                message
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+            <label for="name" class="form-group">
+                <input type="text" class="form-control">
+                <span class="form-group_name">Name</span>
+                <span class="border"></span>
+            </label>
+            <label for="email" class="form-group">
+                <input type="email" class="form-control">
+                <span class="form-group_name">Email</span>
+                <span class="border"></span>
+            </label>
+            <label for="phone" class="form-group">
+                <input type="text" class="form-control">
+                <span class="form-group_name">Phone</span>
+                <span class="border"></span>
+            </label>
+            <label for="company" class="form-group">
+                <input type="text" class="form-control">
+                <span class="form-group_name">Company</span>
+                <span class="border"></span>
+            </label>
+            <label for="website" class="form-group">
+                <input type="text" class="form-control">
+                <span class="form-group_name">Website</span>
+                <span class="border"></span>
+            </label>
+            <label for="message" class="form-group">
+                <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                <span class="form-group_name">Message</span>
+                <span class="border"></span>
             </label>
             <input type="submit" name="" id="">
-        </div>
+        </form>
     </div>
     <h5>Donly Wilson</h5>
     <h6>Web Developer</h6>
