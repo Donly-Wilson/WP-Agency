220f4988f21c0d10d7287a983a49d1f9483a891a
Donly Wilson <donly6@hotmail.com>
2021-02-02 07:46:16 -0400

    complete footer

--- footer.php
+++ footer.php
@@ -1,76 +1,79 @@
 <footer>
-    <div class="title">
-        <h1>Contact</h1>
-    </div>
-    <div class="contact">
-        <div class="contact-info">
-            <div class="email">
-                <span>Email</span>
-                <span>newera@agencey.com</span>
-            </div>
-            <div class="phone">
-                <span>Phone</span>
-                <span>+1 758 230 9214</span>
-            </div>
-            <div class="address">
-                <span>Address</span>
-                <span>Maigot bay, Kanel, 5th</span>
-            </div>
-            <div class="social-container">
-                <a href="http://www.facebook.com/">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a href="http://www.dribbble.com/">
-                    <i class="fab fa-dribbble"></i>
-                </a>
-                <a href="http://www.facebook.com/">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="http://www.facebook.com/">
-                    <i class="fab fa-instagram"></i>
-                </a>
-            </div>
+    <div class="container">
+        <div class="title">
+            <h1>Contact</h1>
         </div>
-        <form class="contact-form">
-            <h3>Contact Us</h3>
-            <!-- contact 7 form -->
-            <?php
-            // echo do_shortcode(
-            //     '[contact-form-7 id="12" title="Contact form 1"]'
-            // );
-            ?>
-            <label for="name" class="form-group">
-                <input type="text" class="form-control">
-                <span class="form-group_name">Name</span>
-                <span class="border"></span>
-            </label>
-            <label for="email" class="form-group">
-                <input type="email" class="form-control">
-                <span class="form-group_name">Email</span>
-                <span class="border"></span>
-            </label>
-            <label for="phone" class="form-group">
-                <input type="text" class="form-control">
-                <span class="form-group_name">Phone</span>
-                <span class="border"></span>
-            </label>
-            <label for="company" class="form-group">
-                <input type="text" class="form-control">
-                <span class="form-group_name">Company</span>
-                <span class="border"></span>
-            </label>
-            <label for="website" class="form-group">
-                <input type="text" class="form-control">
-                <span class="form-group_name">Website</span>
-                <span class="border"></span>
-            </label>
-            <label for="message" class="form-group">
-                <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
-                <span class="form-group_name">Message</span>
-                <span class="border"></span>
-            </label>
-            <input type="submit" name="" id="">
-        </form>
+        <div class="contact">
+            <div class="contact-info">
+                <div class="email">
+                    <span>Email</span>
+                    <span>newera@agencey.com</span>
+                </div>
+                <div class="phone">
+                    <span>Phone</span>
+                    <span>+1 758 230 9214</span>
+                    <span>+44589002932</span>
+                </div>
+                <div class="address">
+                    <span>Address</span>
+                    <span>Maigot bay, Kanel, 5th Avenue Broadway Classtown </span>
+                </div>
+                <div class="social-container">
+                    <a href="http://www.facebook.com/">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="http://www.dribbble.com/">
+                        <i class="fab fa-dribbble"></i>
+                    </a>
+                    <a href="http://www.facebook.com/">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="http://www.facebook.com/">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+            <form class="contact-form">
+                <h3>Contact Us</h3>
+                <!-- contact 7 form -->
+                <?php
+                // echo do_shortcode(
+                //     '[contact-form-7 id="12" title="Contact form 1"]'
+                // );
+                ?>
+                <label for="name" class="form-group">
+                    <input type="text" class="form-control">
+                    <span class="form-group_name">Name</span>
+                    <span class="border"></span>
+                </label>
+                <label for="email" class="form-group">
+                    <input type="email" class="form-control">
+                    <span class="form-group_name">Email</span>
+                    <span class="border"></span>
+                </label>
+                <label for="phone" class="form-group">
+                    <input type="text" class="form-control">
+                    <span class="form-group_name">Phone</span>
+                    <span class="border"></span>
+                </label>
+                <label for="company" class="form-group">
+                    <input type="text" class="form-control">
+                    <span class="form-group_name">Company</span>
+                    <span class="border"></span>
+                </label>
+                <label for="website" class="form-group">
+                    <input type="text" class="form-control">
+                    <span class="form-group_name">Website</span>
+                    <span class="border"></span>
+                </label>
+                <label for="message" class="form-group">
+                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                    <span class="form-group_name">Message</span>
+                    <span class="border"></span>
+                </label>
+                <input type="submit" name="" id="">
+            </form>
+        </div>
     </div>
     <h5>Donly Wilson</h5>
     <h6>Web Developer</h6>
