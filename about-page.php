eda6e2eb4e42971292643ca593bed075f93a1b8e
Donly Wilson <donly6@hotmail.com>
2021-04-07 21:23:56 -0400

    add photo for team

--- about-page.php
+++ about-page.php
@@ -37,10 +37,33 @@ get_header();
             <?php
             $name = $myTeamPod->field('member_name');
             $role = $myTeamPod->field('member_role');
+
+            // returns image to use directly in background
+            $row = $myTeamPod->row();
+            $post_id = $row['ID'];
+            if (!function_exists('get_post_featured_image')) {
+            function get_post_featured_image($post_id, $size)
+            {
+                $return_array = [];
+                $image_id = get_post_thumbnail_id($post_id);
+                $image = wp_get_attachment_image_src($image_id, $size);
+                $image_url = $image[0];
+                $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', true);
+                $image_post = get_post($image_id);
+                $image_caption = $image_post->post_excerpt;
+                $image_description = $image_post->post_content;
+                $return_array['id'] = $image_id;
+                $return_array['url'] = $image_url;
+                $return_array['alt'] = $image_alt;
+                $return_array['caption'] = $image_caption;
+                $return_array['description'] = $image_description;
+                return $return_array;
+            }
+            }
+            $image_properties = get_post_featured_image($post_id, 'full');
             ?>
                 <div class="team-gallery_photo">
-                    <div class="photo" style="background: url(http://localhost/wp-content/uploads/2021/01/Nikeceo.jpg); background-repeat: no-repeat; background-size: cover;">
-                    <!-- <img src="" alt=""> -->
+                    <div class="photo" style="background: url(<?php echo $image_properties['url'] ?>); background-repeat: no-repeat; background-size: cover;">
                     </div>
                     <div class="photo-info">
                         <span class="name"><?php echo $name?></span>
